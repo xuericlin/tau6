24087d5a54e637d5f1c610b6e54ba77de79b80cb
Katherine Yee <kyee2020@mit.edu>
2020-05-05 12:35:16 -0500

    Final update changing button adn adding advice

--- GUI.py
+++ GUI.py
@@ -16,51 +16,79 @@ def updatecycle(guiref, model, queue):
     while True:
         msg = queue.get()
         if msg == Messages.RECORD:
-            guiref.label.set("Recording")
+            guiref.label.set("Recording! Please wait...\n\n")
+            guiref.button.set("...")
             fs = 44100  # Sample rate
-            seconds = 10  # Duration of recording
+            seconds = 45  # Duration of recording
             myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
             sd.wait()  # Wait until recording is finished
-            guiref.label.set("Analyzing recording")
+            guiref.label.set("Analyzing your recording...\n\n")
             write('output.wav', fs, myrecording)
             m = "output"
             p =r"C:\Users\katey\Desktop\6.835\FinalProject\tau6"
             try:
-                sr = "your rate of speech: " + my.myspsr(m,p) +" syllables/sec"
+                mysr = my.myspsr(m,p)
+                sr = "your rate of speech: " + mysr +" syllables/sec"
+                mysr = int(mysr)
+                if mysr <2:
+                    sradv = "You're speaking rather slowly.\nSpeed up a bit to speak more fluidly."
+                elif mysr >3:
+                    sradv = "You're going a bit quickly.\nSlow down a bit to be more understandable."
+                else:
+                    sradv = "Nice pace!\nSpeaking more measuredly can show determination, while speaking more quickly can show passion."
             except:
                 sr = "your rate of speech: n/a"
+                sradv = "Try again.\nSpeaking more measuredly can show determination, while speaking more quickly can show passion."
             try:
-                atc = "your rate of articulation: " +my.myspatc(m,p) +" syllables/sec"
+                myar = my.myspatc(m,p)
+                atc = "your rate of articulation: " +myar +" syllables/sec"
+                myar = int(myar)
+                if myar < 4:
+                    aradv = "You're individual words are a bit slow.\nYou can speed up for easier processing."
+                elif myar >5:
+                    aradv = "You're individual words are a bit fast.\nSlow down and enunciate bettter for improved understanding."
+                else:
+                    aradv = "What clear articulation!\nYou have very understandable enunciation."
             except:
                 atc = "your rate of articulation: n/a"
-##            try:
-##                paus = "number of pauses during speech: " +my.mysppaus(m,p)
-##            except:
-##                paus = "number of pauses: n/a"
+                aradv = "Try again.\nClear articulation and enunciation will help people understand your speech much better."
+#            try:
+#                paus = "number of pauses during speech: " +my.mysppaus(m,p)
+#            except:
+#                paus = "number of pauses: n/a"
             try:
-                bala = "your ratio of time speaking to pausing: " + my.myspbala(m,p)
+                mybal = my.myspbala(m,p)
+                bala = "your ratio of time speaking to pausing: " + mybal
+                mybal = float(mybal)
+                if mybal <.6:
+                    baladv = "You're pausing quite a bit during your speech.\nPausing less can still give your speech weight, while also soundling less choppy."
+                elif mybal>.8:
+                    baladv = "You're barely pauing at all while speaking.\nTakes some breathes for air in order to let you audience process your words."
+                else:
+                    baladv = "Nice balance of pausing to speaking.\nTaking your time with your words increases their weight, while still following the thread of your intentions."
             except:
                 bala = "your ratio of speech to pauses: n/a"
-            sr+="\n Obama state adress: 2 syl/sec\nWeather report: 3 syl/sec\nfootball pep talk: 3 syl/sec"
-            atc+="\n Obama state adress: 4 syl/sec\nWeather report: 4 syl/sec\nfootball pep talk: 5 syl/sec"
-            bala+="\n Obama state adress: .6 \nWeather report: .8\nfootball pep talk: .6"
-            analysis = "\n\n"+sr+"\n\n"+atc+"\n\n"+bala
+                baladv = "Try again. Taking your time with your words increases their weight, while still following the thread of your intentions."
+            sr+="\n Obama state adress: 2 syl/sec\nweather report: 3 syl/sec\nfootball pep talk: 3 syl/sec"
+            atc+="\n Obama state adress: 4 syl/sec\nweather report: 4 syl/sec\nfootball pep talk: 5 syl/sec"
+            bala+="\n Obama state adress: .6 \nweather report: .8\nfootball pep talk: .6"
+            analysis = sr+"\n\n"+sradv+"\n\n"+atc+"\n\n"+aradv+"\n\n"+bala+"\n\n"+baladv+"\n\nPress the record button to try again."
             guiref.label.set(analysis)
+            guiref.button.set("Record")
             
 
 def gui(root, queue):
     label = tk.StringVar()
-    label.set("Waiting for sound recording")
-    recording = tk.BooleanVar()
-    recording.set(False)
+    label.set("Waiting for sound recording\n\n")
+    button = tk.StringVar()
+    button.set("Record")
 
     root.title("TAU6")
-    root.geometry("500x600")
+
+    tk.Button(root, font=("Helvetica", 12), textvariable=button, command=lambda : queue.put(Messages.RECORD)).pack()
+    tk.Label(root, font=("Helvetica", 13), textvariable=label).pack()
     
-    tk.Button(root, font=("Helvetica", 12), text="Record", command=lambda : queue.put(Messages.RECORD)).pack()
-    tk.Label(root, font=("Helvetica", 12), textvariable=label).pack()
-    
-    return SimpleNamespace(label=label)
+    return SimpleNamespace(label=label, button=button)
     
 
 if __name__ == '__main__':
--- myspsolution.py
+++ myspsolution.py
@@ -51,7 +51,7 @@ def myspsr(m,p):
     path=p+"/"
     try:
         objects= run_file(sourcerun, -20, 2, 0.3, "yes",sound,path, 80, 400, 0.01, capture_output=True)
-        print (objects[0]) # This will print the info from the sound object, and objects[0] is a parselmouth.Sound object
+        #print (objects[0]) # This will print the info from the sound object, and objects[0] is a parselmouth.Sound object
         z1=str( objects[1]) # This will print the info from the textgrid object, and objects[1] is a parselmouth.Data object with a TextGrid inside
         z2=z1.strip().split()
         z3=int(z2[2]) # will be the integer number 10
@@ -69,13 +69,13 @@ def myspatc(m,p):
     path=p+"/"
     try:
         objects= run_file(sourcerun, -20, 2, 0.3, "yes",sound,path, 80, 400, 0.01, capture_output=True)
-        print (objects[0]) # This will print the info from the sound object, and objects[0] is a parselmouth.Sound object
+        #print (objects[0]) # This will print the info from the sound object, and objects[0] is a parselmouth.Sound object
         z1=str( objects[1]) # This will print the info from the textgrid object, and objects[1] is a parselmouth.Data object with a TextGrid inside
         z2=z1.strip().split()
         z3=int(z2[3]) # will be the integer number 10
         z4=float(z2[3]) # will be the floating point number 8.3
        #print ("articulation_rate=",z3,"# syllables/sec speaking duration")
-        return str(z4)
+        return str(z3)
     except:
         z3=0
         print ("Try again the sound of the audio was not clear")
@@ -123,7 +123,7 @@ def myspbala(m,p):
     path=p+"/"
     try:
         objects= run_file(sourcerun, -20, 2, 0.3, "yes",sound,path, 80, 400, 0.01, capture_output=True)
-        print (objects[0]) # This will print the info from the sound object, and objects[0] is a parselmouth.Sound object
+        #print (objects[0]) # This will print the info from the sound object, and objects[0] is a parselmouth.Sound object
         z1=str( objects[1]) # This will print the info from the textgrid object, and objects[1] is a parselmouth.Data object with a TextGrid inside
         z2=z1.strip().split()
         z3=int(z2[3]) # will be the integer number 10
